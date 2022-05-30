ddca6b991cd507cf01329d214b6839fd86cdc3a7
Richard <hualuoyu_yks@qq.com>
2022-05-30 09:17:51 +0800

    增加ESI调用工具类

---
+++ src/main/java/xyz/foolcat/eve/evehelper/client/EsiNormalClient.java
@@ -0,0 +1,100 @@
+package xyz.foolcat.eve.evehelper.client;
+
+import com.dtflys.forest.annotation.*;
+import com.dtflys.forest.callback.OnError;
+import xyz.foolcat.eve.evehelper.domain.system.MarketOrder;
+import xyz.foolcat.eve.evehelper.dto.esi.AuthTokenResponseDTO;
+import xyz.foolcat.eve.evehelper.dto.esi.CharactorInfoResponseDTO;
+import xyz.foolcat.eve.evehelper.dto.esi.UniverseNameResponeDTO;
+import xyz.foolcat.eve.evehelper.interceptor.EsiCharactorInterceptor;
+
+import java.util.List;
+
+/**
+ * 国服ESI调用
+ * <p>
+ * 用于调用网易提供的各种ESI接口
+ *
+ * @author Leojan
+ * @date 2021-12-07 17:19
+ */
+@BaseRequest(
+        baseURL = "https://esi.evepc.163.com/latest",     // 默认域名
+        headers = {
+                "Accept:application/json; charset=UTF-8",// 默认请求头
+                "Cache-Control: no-cache",
+        },
+        interceptor = EsiCharactorInterceptor.class
+)
+public interface EsiNormalClient {
+
+    /**
+     * code认证
+     *
+     * @param grantType
+     * @param code
+     * @param clientId
+     */
+    @PostRequest(
+            url = "https://login.evepc.163.com/v2/oauth/token",
+            headers = {
+                    "Content-Type: application/x-www-form-urlencoded",
+                    "Host: login.evepc.163.com",
+            }
+    )
+    AuthTokenResponseDTO addCharactorAuth(@Body("grant_type") String grantType, @Body("code") String code, @Body("client_id") String clientId);
+
+    /**
+     * refresh_token认证
+     *
+     * @param grantType
+     * @param refreshToken
+     * @param clientId
+     */
+    @PostRequest(
+            url = "https://login.evepc.163.com/v2/oauth/token",
+            headers = {
+                    "Content-Type: application/x-www-form-urlencoded",
+                    "Host: login.evepc.163.com"
+            }
+    )
+    AuthTokenResponseDTO getAccessToken(@Body("grant_type") String grantType, @Body("refresh_token") String refreshToken, @Body("client_id") String clientId, OnError onError);
+
+
+    /**
+     * 获取角色信息
+     *
+     * @param charactorId
+     * @return
+     */
+    @Get("/characters/{charactor_id}")
+    CharactorInfoResponseDTO getCharactorInfo(@Var("charactor_id") String charactorId);
+
+    /**
+     * 获取军团信息
+     *
+     * @param corporationId
+     * @return
+     */
+    @Get("/corporations/{corporation_id}")
+    CharactorInfoResponseDTO getCorporationInfo(@Var("corporation_id") String corporationId);
+
+    /**
+     * 获取item名称
+     *
+     * @param itemIds
+     * @return
+     */
+    @Post("/universe/names")
+    List<UniverseNameResponeDTO> getUniverseName(@JSONBody List itemIds);
+
+    /**
+     * 获取星域市场订单
+     * @param regionId
+     * @param page
+     * @return
+     */
+    @Get("/markets/{region_id}/orders/")
+    List<MarketOrder> getMarketOrder(@Var("region_id") String regionId,@Query("page") Integer page,@Query("type_id") Integer typeId);
+
+}
