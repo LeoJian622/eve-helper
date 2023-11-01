6adcd5375bf19bc367bfef5d288c46401d20608e
Rihcard <hualuoyu_yks@qq.com>
2023-11-01 16:48:14 +0800

    1)邮件接口封装完成 2)增加相关ESI接口的test

---
+++ src/test/java/xyz/foolcat/eve/evehelper/esi/api/MailApiTest.java
@@ -0,0 +1,86 @@
+package xyz.foolcat.eve.evehelper.esi.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Mono;
+import xyz.foolcat.eve.evehelper.domain.system.EveAccount;
+import xyz.foolcat.eve.evehelper.esi.auth.AuthorizeOAuth;
+import xyz.foolcat.eve.evehelper.esi.auth.GrantType;
+import xyz.foolcat.eve.evehelper.esi.model.AuthTokenResponse;
+import xyz.foolcat.eve.evehelper.esi.model.MailResponse;
+import xyz.foolcat.eve.evehelper.esi.model.RequestedMailResponse;
+import xyz.foolcat.eve.evehelper.esi.model.send.NewMail;
+import xyz.foolcat.eve.evehelper.esi.model.sub.Recipient;
+import xyz.foolcat.eve.evehelper.service.system.EveAccountService;
+
+import java.util.List;
+import java.util.Objects;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@DisplayName("ESI Mail Api Test")
+class MailApiTest {
+
+    @Autowired
+    MailApi mailApi;
+
+    @Autowired
+    EveAccountService eveAccountService;
+
+    @Autowired
+    AuthorizeOAuth authorizeOAuth;
+
+    String at = "Bearer ";
+
+    @BeforeEach
+    void initAccessToken() {
+        EveAccount entity = eveAccountService.lambdaQuery().eq(EveAccount::getId, 3).one();
+        Mono<AuthTokenResponse> authTokenResponseMono = authorizeOAuth.updateAccessToken(GrantType.REFRESH_TOKEN, entity.getRefreshToken());
+        at = at + Objects.requireNonNull(authTokenResponseMono.block()).getAccessToken();
+        System.out.println("at = " + at);
+    }
+
+    @Test
+    void queryCharacterMails() {
+        List<RequestedMailResponse> requestedMailResponses = mailApi.queryCharacterMails(2112818290, "serenity", null, null, at).collectList().block();
+        System.out.println("requestedMailResponses = " + requestedMailResponses);
+    }
+
+    @Test
+    void addCharacterMail() {
+        NewMail newMail = new NewMail();
+        newMail.setSubject("test");
+        newMail.setBody("新邮件测试");
+
+        Recipient recipient = new Recipient();
+        recipient.setRecipientId(2112818290);
+        recipient.setRecipientType("character");
+        newMail.setRecipients(List.of(recipient));
+
+        Integer integer = mailApi.addCharacterMail(2112818290, "serenity", newMail, at).block();
+        System.out.println("integer = " + integer);
+    }
+
+    @Test
+    void deleteCharacterMail() {
+        Object o = mailApi.deleteCharacterMail(2112818290, "serenity", 79794658, at).block();
+        System.out.println("o = " + o);
+    }
+
+    @Test
+    void queryCharacterMail() {
+        MailResponse mailResponse = mailApi.queryCharacterMail(2112818290, "serenity", 79718162, at).block();
+        System.out.println("mailResponse = " + mailResponse);
+    }
+
+    @Test
+    void updateCharacterMail() {
+        Object o = mailApi.updateCharacterMail(2112818290, "serenity", 79794658, List.of(), true, at).block();
+        System.out.println("o = " + o);
+    }
+}
