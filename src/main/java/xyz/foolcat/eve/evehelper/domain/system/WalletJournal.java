35573b3ea4a63bb47faf6ee0b210dfed2c4d67b3
Rihcard <hualuoyu_yks@qq.com>
2024-11-18 11:11:13 +0800

    1)存个档

--- src/main/java/xyz/foolcat/eve/evehelper/domain/system/WalletJournal.java
+++ src/main/java/xyz/foolcat/eve/evehelper/domain/system/WalletJournal.java
@@ -5,9 +5,10 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.Date;
-import lombok.Data;
 
 @Schema
 @Data
